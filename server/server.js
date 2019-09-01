f5c49a39ac0c76c5747fd3f4c7a502954e6716c7
win7master <win7master@gmail.com>
2019-09-01 14:58:59 +0300

    server update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const ioHook = require('iohook');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+// const debounce = require('lodash.debounce');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,17 +12,25 @@ const io = socketIO(server);
 io.on('connection', socket => {
     console.log('new user connected');
 
-    ioHook.on('keydown', click => socket.emit('message', click));
+    ioHook.on('keydown', press => {
+        socket.emit('key-message', press)
+    });
+
+    // ioHook.on('mousemove', move => {
+        // debounce(() => socket.emit('mouse-message', move), 100, {leading: false, trailing: true});
+    //     socket.emit('mouse-message', move);
+    // });
+
+    ioHook.on('mousedrag', drag => {
+        socket.emit('mouse-message', drag);
+    });
+ 
+    ioHook.on('mouseclick', click => {
+        socket.emit('mouse-message', click);
+    });
+
     socket.on('disconnect', () => console.log('User was disconnected'));
 });
 
 ioHook.start();
 server.listen(3000, () => console.log('listening on PORT: ', 3000));
-
-
-
-
-
-
-// ioHook.on('mousemove', event => {
-// });
